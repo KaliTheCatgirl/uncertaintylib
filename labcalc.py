055142ce31131bad5779e4dc1b473bace53a2ff9
KaliTheCatgirl <77519462+KaliTheCatgirl@users.noreply.github.com>
2024-11-06 16:21:56 -0500

    Add overall calculations (trial-independent)

--- labcalc.py
+++ labcalc.py
@@ -12,8 +12,9 @@ if __name__ == "__main__":
     cl_molar_mass = 35.45 # g/mol
     moles_agcl = precipitate_mass / agcl_molar_mass # mol
     cl_mass = moles_agcl * cl_molar_mass # g
+    cl_content_1 = cl_mass / salt_sample_mass
     print(f"Mass of chlorine: {cl_mass}g")
-    print(f"Chlorine mass percentage: {cl_mass / salt_sample_mass * 100}%")
+    print(f"Chlorine mass percentage: {cl_content_1 * 100}%")
     
     print()
     
@@ -27,5 +28,14 @@ if __name__ == "__main__":
     cl_molar_mass = 35.45 # g/mol
     moles_agcl = precipitate_mass / agcl_molar_mass # mol
     cl_mass = moles_agcl * cl_molar_mass # g
+    cl_content_2 = cl_mass / salt_sample_mass
     print(f"Mass of chlorine: {cl_mass}g")
-    print(f"Chlorine mass percentage: {cl_mass / salt_sample_mass * 100}%")
+    print(f"Chlorine mass percentage: {cl_content_2 * 100}%")
+
+    print()
+
+    avg_content = (cl_content_1 + cl_content_2) / 2
+    expected = 54.1
+    print(f"Average chlorine mass percentage: {avg_content * 100}%")
+    print(f"Relative error: {((avg_content * 100 - expected) / expected).abs() * 100}%")
+    print(f"Relative spread: {(cl_content_2 - cl_content_1) / avg_content * 1000}ppt")
